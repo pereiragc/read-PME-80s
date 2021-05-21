e6af063aa0d1e3501705d67f5faa5c1943320307
Gustavo <gustavo.alt1@gmail.com>
2021-05-21 14:42:42 -0400

    repurpose repo to only read 84-90

--- lib.r
+++ lib.r
@@ -1,43 +1,36 @@
 #' Generates table with year / state / file path from user settings
 generatePathTables <- function(parameters) {
-  lreadraw <- lapply(parameters$batch_subpaths, function(subpath) {
-    list.files(file.path(parameters$base_data_path, subpath),
-               recursive = TRUE, full.names = TRUE)
-  })
+  lreadraw <- list.files(file.path(parameters$base_data_path),
+                         recursive = TRUE, full.names = TRUE)
 
-  if (!is.null(lreadraw$pme80_90)) {
-    ## Recover relevant files through .DAT extension
+  ## Recover relevant files through .DAT extension
+  paths <- grep(".dat$", lreadraw,
+                      ignore.case = TRUE, value = TRUE)
 
-    paths_80_90 <- grep(".dat$", lreadraw$pme80_90,
-                        ignore.case = TRUE, value = TRUE)
+  ## Infer state and year via the "PME{year}{state}.DAT" format
+  fname_regex <- ".*pme(\\d{2})(\\w{2})\\d?\\.dat"
 
-    ## Infer state and year via the "PME{year}{state}.DAT" format
-    regex_80_90 <- ".*pme(\\d{2})(\\w{2})\\d?\\.dat"
+  dt_paths <-
+    list(
+      fname = paths,
+      year = paste0("19",
+                    gsub(fname_regex, "\\1",
+                         paths, ignore.case = TRUE)),
+      state = gsub(fname_regex, "\\2", paths, ignore.case = TRUE)
+    )
 
-    dt_paths_80_90 <-
-      list(
-        path = paths_80_90,
-        year = paste0("19",
-                      gsub(regex_80_90, "\\1",
-                           paths_80_90, ignore.case = TRUE)),
-        state = gsub(regex_80_90, "\\2", paths_80_90, ignore.case = TRUE)
-      )
-  }
-
-  return(list(
-    pme80_90 = data.table::as.data.table(dt_paths_80_90)
-  ))
+  return(data.table::as.data.table(dt_paths))
 }
 
 #' Uses path table generated from `generatePathTables` to read raw files
-readYearRaw_80_90 <- function(yyyy, dt_path) {
+readYearRaw <- function(yyyy, dt_path, .internal_params) {
   dt_raw <- dt_path[year == as.character(yyyy),
-                    readRaw(path, year, state), path]
+                    readRaw(fname, year, state, .internal_params), fname]
 }
 
 
-readRaw <- function(path, year, state) {
-  dt <- data.table::fread(path, header = FALSE, sep = "$",
+readRaw <- function(fname, year, state, .internal_params) {
+  dt <- data.table::fread(fname, header = FALSE, sep = "$",
                           strip.white = FALSE)
 
   if (ncol(dt) > 1) {
@@ -46,6 +39,114 @@ readRaw <- function(path, year, state) {
 
   data.table::setnames(dt, 1, "full_line")
   dt[, `:=`(year = as.integer(year),
-            path = path,
+            fname = fname,
             state = state)]
 }
+
+
+
+readDict <- function(yyyy, parameters, .internal_params) {
+  lf <- list.files(.internal_params$dict_path,
+                   pattern = as.character(yyyy),
+                   full.names = TRUE)
+  if (length(lf) != 1) stop("[readDict] Ambiguous dictionary specification")
+
+  dict <- parseDictionary(yyyy, lf, parameters)
+  dict[]
+}
+
+parseDictionary <- function(yyyy, fname, parameters) {
+  dt_input <- unique(data.table::fread(fname, header = FALSE,
+                                       encoding = "Latin-1"))
+
+
+  cnames_hint <- c("VARI", "NOME", "DESDE", "TAM")
+                                        # ^ These names should be present on ALL
+                                        # years
+
+  success <- setCorrectNames(dt_input, cnames_hint)
+                                        # changes in place!! (& returns a
+                                        # code indicating success)
+  if (!success) {
+    ## TODO: log
+    return(NULL)
+  }
+
+
+  ## Tidy up
+  cnames_tidy <- c("Name", "Descr", "Pos", "Width")
+  data.table::setnames(dt_input, cnames_hint, cnames_tidy)
+  dt_input <- dt_input[, .(Name, Descr, Pos, Width)]
+
+
+  ## Get relevant entries
+  dt_input <- dt_input[grepl("[0-9]+", Name)]
+
+  ## Append "V" in front of variable names if needed
+  dt_input[grepl("^[^V]", Name), Name := paste("V", Name, sep="")]
+
+
+  ## Remove duplicate names (keep first)
+  dt_input[, n_appear := seq_len(.N), Name]
+  dt_input <- dt_input[n_appear == 1]
+  dt_input[, n_appear := NULL]
+
+
+  ## Compute Start/End positions
+  dt_input[, `:=`(Pos = as.integer(Pos),
+                  Width = as.integer(Width))]
+  dt_input[, `:=`(Start = Pos, End = Pos + Width - 1)]
+
+  if (parameters$save_parsed_dictionaries) {
+    data.table::fwrite(dt_input,
+                       glue("./output/treated_{basename(fname)}"))
+  }
+  return(dt_input)
+}
+
+
+
+##' Corrects names in
+setCorrectNames <- function(dt_input, cnames_hint) {
+  ## Context: the header of `dt_input` is in some of the rows of `dt_input`.
+  ##
+  ## I know the header *must* contain the column names in `cnames_hint`. So
+  ## the idea of this function is:
+  ##
+  ## - sweep rows of `dt_input` trying to find which one contains all the column
+  ##   names in `cnames_hint` (this is the `apply` step below)
+  ##
+  ## - there might be no row containing `cnames_hint` (in which case the
+  ##   dictionary file is considered defectuous, and the function returns a
+  ##   "bad" code indicating failure)
+  ##
+  ## - there might be multiple rows containing `cnames_hint`. I pick the first
+  ##   one. Names outside `cnames_hint` may be blank, for example, in which case
+  ##   that would not be a valid header. What I do in that case is to only
+  ##   change the non-blank column names of `dt_input`.
+  ##
+  ##   There are obvious heuristics that would implement the same goal more
+  ##   robustly. Since it's not useful for my purposes, I leave this for future
+  ##   PRs :D
+
+
+  find_rows <- which(apply(dt_input, 1, function(x) all(cnames_hint %in% x)))
+
+  if (length(find_rows) == 0) return(FALSE) # return bad code
+
+
+  candidate_header <- unlist(dt_input[find_rows[1]])
+
+
+  ## "Non-empty" column names (by definition are a superset of `cnames_hint`)
+  indices_nonempty <-
+    Filter(function(i) nchar(trimws(candidate_header[i])) > 0,
+           seq_along(candidate_header))
+
+
+  setnames(dt_input, colnames(dt_input)[indices_nonempty],
+           candidate_header[indices_nonempty])
+
+  return(TRUE)
+}
+
